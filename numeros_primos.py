c1a7d5975466f7aae3ed00bd57186411eef76379
minoreh <minoreherrera@gmail.com>
2016-08-04 17:25:20 -0600

    validaciones
    num cero or primo

--- numeros_primos.py
+++ numeros_primos.py
@@ -4,9 +4,11 @@ fin=0
 while fin<2:
     print("Por favor digita un numero entero: ")
     num= input()
-
     if num.isdigit():
         num = int(num)
+        if int(num) == 0:
+            print("ERROR: el numero digitado NO debe ser CERO")
+            break
         for i in range(1, num + 1):
             if (num % i == 0):
                 a = a + 1
@@ -21,6 +23,8 @@ while fin<2:
         else:
             print(str(num) + " es un numero impar")
             pass
+
     else:
         print(">ERROR: DEBE INGRESAR UN NUMERO ENTERO")
+        print(">NO DEBE INGRESAR NUMEROS NEGATIVOS O CARACTERES")
         fin=1
