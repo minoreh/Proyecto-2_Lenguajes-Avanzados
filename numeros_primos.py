a90afc18b8e882a0ea40ab3d9cfd9bc77440f3a0
minoreh <minoreherrera@gmail.com>
2016-08-03 21:39:58 -0600

    validaciones y numeros
    se debe mostrar al usuario un mensaje indicando si el número es par o
    impar, o si es o no es, un número primo. Se debe validar que el usuario
    no ingrese letras o caracteres

--- numeros_primos.py
+++ numeros_primos.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+a=0
 fin=0
 while fin<2:
     print("Por favor digita un numero entero: ")
@@ -7,7 +7,20 @@ while fin<2:
 
     if num.isdigit():
         num = int(num)
-        break
+        for i in range(1, num + 1):
+            if (num % i == 0):
+                a = a + 1
+        if (a != 2):
+            print(str(num) +" Tu numero No es primo")
+        else:
+            print(str(num) +" Tu numero Si es primo")
+        pass
+        if (num % 2 == 0):
+            print(str(num) + " es un numero par")
+            pass
+        else:
+            print(str(num) + " es un numero impar")
+            pass
     else:
-        print("ERROR: DEBE INGRESAR UN NUMERO ENTERO")
+        print(">ERROR: DEBE INGRESAR UN NUMERO ENTERO")
         fin=1
